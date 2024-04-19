26b8005d2caf69379f81980e7b093adcc281ea30
Zeel Patel <68885863+ZeelPatel18@users.noreply.github.com>
2024-04-19 16:53:31 -0400

    Update provisioner.tf

--- tf-files/modules/vmlinux-n01660400/provisioner.tf
+++ tf-files/modules/vmlinux-n01660400/provisioner.tf
@@ -5,6 +5,6 @@ resource "null_resource" "ansible" {
   }
   depends_on = [ azurerm_linux_virtual_machine.vm-linux ]
   provisioner "local-exec" {
-    command = "cd ../ansible && env /usr/bin/arch -arm64 ansible-playbook n01613688-playbook.yaml"
+    command = "cd ../ansible && env /usr/bin/arch -arm64 ansible-playbook n01660400-playbook.yaml"
   }
 }
