a2b67153ccb5ecdd934d4f97be17fec2e7a85544
Zeel Patel <68885863+ZeelPatel18@users.noreply.github.com>
2024-04-19 16:02:58 -0400

    Update provisioner.tf

--- tf-files/modules/vmlinux-n01660400/provisioner.tf
+++ tf-files/modules/vmlinux-n01660400/provisioner.tf
@@ -1,15 +1,10 @@
-resource "null_resource" "display_hostnames" {
-  count      = var.vm_count
-  depends_on = [azurerm_linux_virtual_machine.vm-linux]
-  provisioner "remote-exec" {
-    connection {
-      type        = "ssh"
-      user        = "Japan"
-      private_key = file("${var.vmlinux_priv_key_path}")
-      host        = azurerm_public_ip.linux-ip[count.index].fqdn
-    }
-    inline = [
-      "echo 'The hostname is:' $(hostname)"
-    ]
+resource "null_resource" "ansible" {
+  count = var.vm_count
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  depends_on = [ azurerm_linux_virtual_machine.vm-linux ]
+  provisioner "local-exec" {
+    command = "cd ../ansible && env /usr/bin/arch -arm64 ansible-playbook n01613688-playbook.yaml"
   }
 }
