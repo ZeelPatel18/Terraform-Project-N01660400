f1b53199856d2b88742ab204c0980e92c5040823
Zeel Patel <68885863+ZeelPatel18@users.noreply.github.com>
2024-04-19 15:25:06 -0400

    Delete tf-files directory

--- tf-files/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "n01660400-rg"
-    storage_account_name = "n01660400storage"
-    container_name       = "tfstatefiles"
-    key                  = "terraform.tfstate"
-  }
-}
